fc77ea7fd512fc2c29a83c21783a9c0a192d440c
Michal Hruby <michal.mhr@gmail.com>
2024-11-25 16:48:34 +0400

    move files

--- codegen/generate.go
+++
@@ -1,6 +0,0 @@
-//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.9.2 c/zigzag.c -l -p streamvbyte -o ../ -a amd64 -O3 --suffix _amd64_sse --function-suffix _sse
-//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.9.2 c/zigzag.c -l -p streamvbyte -o ../ -a avx2 -O3 --suffix _amd64_avx2 --function-suffix _avx2
-//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.9.2 c/zigzag.c -l -p streamvbyte -o ../ -a arm64 -O3 --suffix _arm64
-//go:generate go run github.com/mhr3/goruntool@v0.1.1 github.com/mhr3/gocc/cmd/gocc@v0.13.0 c/svb_neon.c -l -p streamvbyte -o ../ -a arm64 -O3 --suffix _arm64
-
-package codegen
